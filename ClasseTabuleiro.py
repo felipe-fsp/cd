32e878dbb083fd0e47fcdaaee21bec62b0ae5a53
GabrielVInsper <bielvilaca@hotmail.com>
2016-04-27 00:44:20 -0300

    alterei a função desenhar
    Adicionei o botão de reiniciar, a label que mostra as vitórias, e a configuração dos botões

--- ClasseTabuleiro.py
+++ ClasseTabuleiro.py
@@ -185,45 +185,54 @@ class Tabuleiro:
     
     
     def desenhar_tabuleiro(self):
-        self.b_1 = tk.Button(self.window,height =8, width =20)
+        self.b_1 = tk.Button(self.window, height = 6, width = 40)
         self.b_1.configure(command=self.bo_1)
-        self.b_1.grid(row=0, column=0,pady=5)
+        self.b_1.grid(row=0, column=0, sticky = "ns")
 
-        self.b_2 = tk.Button(self.window,height =8, width =20)
-        self.b_2.configure(command=lambda: self.b_2.configure(text = self.jogador()))
-        self.b_2.grid(row=1, column=0,padx=5)
+        self.b_2 = tk.Button(self.window, height = 6, width = 40)
+        self.b_2.configure(command=self.bo_2)
+        self.b_2.grid(row=0, column=1, sticky = "ns")
         
-        self.b_3 = tk.Button(self.window,height =8, width =20)
-        self.b_3.configure(command=lambda: self.b_3.configure(text = self.jogador()))
-        self.b_3.grid(row=2, column=0)
+        self.b_3 = tk.Button(self.window, height = 6, width = 40)
+        self.b_3.configure(command=self.bo_3)
+        self.b_3.grid(row=0, column=2, sticky = "ns")
       
-        self.b_4 = tk.Button(self.window,height =8, width =20)
-        self.b_4.configure(command=lambda: self.b_4.configure(text = self.jogador()))
-        self.b_4.grid(row=0, column=1)
+        self.b_4 = tk.Button(self.window, height = 6, width = 40)
+        self.b_4.configure(command=self.bo_4)
+        self.b_4.grid(row=1, column=0, sticky = "ns")
 
-        self.b_5 = tk.Button(self.window,height =8, width =20)
-        self.b_5.configure(command=lambda: self.b_5.configure(text = self.jogador()))
-        self.b_5.grid(row=1, column=1)
+        self.b_5 = tk.Button(self.window, height = 6, width = 40)
+        self.b_5.configure(command=self.bo_5)
+        self.b_5.grid(row=1, column=1, sticky = "ns")
         
-        self.b_6 = tk.Button(self.window,height =8, width =20)
-        self.b_6.configure(command=lambda: self.b_6.configure(text = self.jogador()))
-        self.b_6.grid(row=2, column=1)
+        self.b_6 = tk.Button(self.window, height = 6, width = 40)
+        self.b_6.configure(command=self.bo_6)
+        self.b_6.grid(row=1, column=2, sticky = "ns")
         
-        self.b_7 = tk.Button(self.window,height =8, width =20)
-        self.b_7.configure(command=lambda: self.b_7.configure(text = self.jogador()))
-        self.b_7.grid(row=0, column=2)
-
-        self.b_8 = tk.Button(self.window,height =8, width =20)
-        self.b_8.configure(command=lambda: self.b_8.configure(text = self.jogador()))
-        self.b_8.grid(row=1, column=2, padx =5)
+        self.b_7 = tk.Button(self.window, height = 6, width = 40)
+        self.b_7.configure(command=self.bo_7)        
+        self.b_7.grid(row=2, column=0, sticky = "ns")
         
-        self.b_9 = tk.Button(self.window,height =8, width =20)
-        self.b_9.configure(command=lambda: self.b_9.configure(text = self.jogador()))
-        self.b_9.grid(row=2, column=2,pady=5)
+        self.b_8 = tk.Button(self.window, height = 6, width = 40)
+        self.b_8.configure(command=self.bo_8)
+        self.b_8.grid(row=2, column=1, sticky = "ns")
+        
+        self.b_9 = tk.Button(self.window, height = 6, width = 40)
+        self.b_9.configure(command=self.bo_9)
+        self.b_9.grid(row=2, column=2, sticky = "ns")
         
         self.Prox_Jogada = tk.Label(self.window, font=('Helvetica', 10))
         self.Prox_Jogada.configure(textvariable=self.jogador_nome_var)        
-        self.Prox_Jogada.grid(row=3, column=0, columnspan=1, sticky="nsew", pady=15) 
+        self.Prox_Jogada.grid(row=3, column=0, columnspan=1, rowspan = 1, sticky="nsew") 
+        
+        self.Restart_button = tk.Button(self.window)
+        self.Restart_button.configure(command=self.limpar_tela, text = "Reiniciar Jogo")
+        self.Restart_button.grid(row=3, column=2, columnspan=1, rowspan = 1, sticky="nsew")         
+        
+        self.Main_label = tk.Label(self.window, font=('Helvetica', 10))
+        self.Main_label.configure(textvariable = self.label_principal_show , bg = "white", fg = "blue")        
+        self.Main_label.grid(row=4, column=0, columnspan=3, rowspan = 1, sticky="nsew") 
+    
         
     def jogador(self):
         if self.jogador_id == 0:
