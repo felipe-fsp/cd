b95949f711a85526779703916909e9d91c4d72e6
fspoficial <felipe@speedio.com.br>
2016-04-26 22:14:06 -0300

    Escolhendo quem começa e verificando matriz
    Verificamos que a matriz está zerada e é da forma 3x3. Precisamos lembrar de criar a lógica da função sorteio para escolher aleatoriamente quem começa cada game.

--- jogo.py
+++ jogo.py
@@ -5,6 +5,7 @@ class jogo():
   def __init__(self):
       self.jogadores = ["X","O"," "] #Representa as duas letras tradicionais do Jogo da Velha
       self.vencedor = "*"
+      self.sorteio()
       self.jogador = self.jogadores
       self.tabuleiro = np.zeros([3,3]) #Matriz do jogo
       self.games_X = 0
