6357031c0d88a1bd20591d0e0a4ec7ebe257e2c4
fspoficial <felipe@speedio.com.br>
2016-04-26 22:09:08 -0300

    Criando algoritmo base
    Definimos o tabuleiro e os jogadores no programa inicial

---
+++ jogo.py
@@ -0,0 +1,11 @@
+import numpy as np
+import random
+
+class jogo():
+  def __init__(self):
+      self.jogadores = ["X","O"," "] #Representa as duas letras tradicionais do Jogo da Velha
+      self.vencedor = "*"
+      self.jogador = self.jogadores
+      self.tabuleiro = np.zeros([3,3]) #Matriz do jogo
+      self.games_X = 0
+      self.gamess_O = 0
