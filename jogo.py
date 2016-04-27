67946c0541ebade7a88f5a19a33492a0f027d482
fspoficial <felipe@speedio.com.br>
2016-04-27 01:32:45 -0300

    Verifica vencedor criada
    Criamos uma estrutura que nos permite determinar se houve vencedor após uma jogada no jogo da velha. A ideia do produto é interessante para possíveis jogos da velha maiores(Que não usam matrizes 3x3). Como temos células vazias marcadas como zero, Podemos avaliar por paridade, desde que não haja elementos nulos, se alguém venceu e quem foi!

--- jogo.py
+++ jogo.py
@@ -22,8 +22,48 @@ class jogo():
     	self.jogador=self.jogadores[1]
     
   def recebe_jogada(self,l,c):
+    
   
   def verifica_ganhador(self):
+    produtocoluna = np.prod(self.matriz, 0)
+    produtolinha = np.prod(self.matriz, 1)
+    if (produtocoluna[0] %2 == 0 and produtocoluna[0]>0)
+    or (produtocoluna[1] %2 == 0 and produtocoluna[1]>0)
+    or (produtocoluna[2] %2 == 0 and produtocoluna[2]>0)
+    or (produtolinha[0] %2 == 0 and produtolinha[0]>0)
+    or (produtolinha[1] %2 == 0 and produtolinha[1]>0)
+    or (produtolinha[2] %2 == 0 and produtolinha[2]>0)
+    or (((self.tabuleiro[0][0] * self.tabuleiro[1][1] * self.tabuleiro[2][2]) > 0) and ((self.tabuleiro[0][0] * self.tabuleiro[1][1] * self.tabuleiro[2][2])%2==0))
+    or (((self.tabuleiro[2][0] * self.tabuleiro[1][1] * self.tabuleiro[0][2]) > 0) and ((self.tabuleiro[2][0] * self.tabuleiro[1][1] * self.tabuleiro[0][2])%2==0)): 
+      self.vencedor="O"
+      self.jogador=self.jogadores[2]
+      self.GO+=1
+      print("Jogador O chegou a %d vitórias" % self.GO)
+      return 2
+    
+    
+    if (produtocoluna[0] %2 == 1 and produtocoluna[0]>0)
+    or (produtocoluna[1] %2 == 1 and produtocoluna[1]>0)
+    or (produtocoluna[2] %2 == 1 and produtocoluna[2]>0)
+    or (produtolinha[0] %2 == 1 and produtolinha[0]>0)
+    or (produtolinha[1] %2 == 1 and produtolinha[1]>0)
+    or (produtolinha[2] %2 == 1 and produtolinha[2]>0)
+    or (((self.tabuleiro[0][0] * self.tabuleiro[1][1] * self.tabuleiro[2][2]) > 0) and ((self.tabuleiro[0][0] * self.tabuleiro[1][1] * self.tabuleiro[2][2])%2==1))
+    or (((self.tabuleiro[2][0] * self.tabuleiro[1][1] * self.tabuleiro[0][2]) > 0) and ((self.tabuleiro[2][0] * self.tabuleiro[1][1] * self.tabuleiro[0][2])%2==1)): 
+      self.vencedor="X"
+      self.jogador=self.jogadores[2]
+      self.GX+=1
+      print("Jogador X chegou a %d vitórias" % self.GX)
+      return 1
+      
+    elif np.sum(self.tabuleiro) == 14 or np.sum(self.tabuleiro) == 13:
+      print()
+      self.vencedor = "Empate - Deu velha!"
+      self.jogador = self.jogadores[2]
+      return 0
+    else:
+      return -1
+      
   
   
   def limpa_jogadas(self):
