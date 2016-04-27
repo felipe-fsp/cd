ad714cf35df785591156cfcccf3ec73b62f39941
fspoficial <felipe@speedio.com.br>
2016-04-26 22:26:41 -0300

    Definindo os métodos da classe jogo
    Definimos os métodos da classe jogo, ainda sem implementar a lógica, mas já tendo uma ideia dos parâmetros que vamos usar. 
    Precisamos nos lembrar de entender a função random para deixar o jogo justo(ou ficar revezando quem joga primeiro - Ideia burra)

--- jogo.py
+++ jogo.py
@@ -9,8 +9,18 @@ class jogo():
       self.jogador = self.jogadores
       self.tabuleiro = np.zeros([3,3]) #Matriz do jogo
       self.games_X = 0
-      self.gamess_O = 0
-      
+      self.games_O = 0
+  
+  
+  def jogar(self):
+    
+  def sorteio(self): #Precisamos pensar na lógica em python para sortear, mas vamos usar "random"
+    
+  def recebe_jogada(self,l,c):
+  
+  def verifica_ganhador(self):
+  
+  fel limpa_jogadas(self):
 
 
 
